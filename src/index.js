02f1c46541d9241f306f04c32ad03d8f5bc1001e
DESKTOP-K2F6M1C\A_mah <amahmood561@gmail.com>
2021-05-10 21:38:11 -0400

    Update for initial volt screen and dashboard template react sign in page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Signin from './pages/login/Signin';
+import {HashRouter} from "react-router-dom";
+// core styles
+import "../src/scss/volt.scss";
 
+// vendor styles
+import "@fortawesome/fontawesome-free"
+import "@fortawesome/fontawesome-free/css/all.css";
+import "react-datetime/css/react-datetime.css";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <HashRouter>
+        <Signin />
+    </HashRouter>,
   document.getElementById('root')
 );
-
+// <App />
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
